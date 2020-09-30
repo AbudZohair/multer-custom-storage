60a66b8eb2eff9ad800e52b0fc285fcaa8af1337
Abud Zohair <abud.zohair@gmail.com>
2020-09-30 03:04:10 +0200

    asd

---
+++ config/mutlerConfig.js
@@ -0,0 +1,50 @@
+const multer = require('multer');
+const cloudinaryStorage = require('./cloudinary-storage-multer');
+
+const diskStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './images');
+  },
+  filename: (req, file, cb) => {
+    const uid = Date.now();
+    cb(null, `${uid + '_' + file.originalname.replace(/\s/g, '')}`);
+  }
+});
+
+const fileFileter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb({
+      success: false,
+      msg: `File type isn't supported`
+    });
+  }
+};
+
+const storage = [diskStorage, cloudinaryStorage];
+
+function loadBalanceWrapper() {
+  let c = 0;
+  return function loadBalance() {
+    if (storage.length - 1 === c) {
+      const passStorage = storage[c];
+      c = 0;
+      return passStorage;
+    } else {
+      const passStorage = storage[c];
+      c++;
+      return passStorage;
+    }
+  };
+}
+const loadBalance = loadBalanceWrapper();
+
+module.exports = {
+  fileFileter,
+  loadBalance
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const { fileFileter, loadBalance } = require('./config/mutlerConfig');
+const multer = require('multer');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+const uuid = require('uuid').v4;
+
+db.defaults({ images: {} }).write();
+// create express app
+const app = express();
+
+// add other middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/upload', (req, res, next) => {
+  const upload = multer({
+    storage: loadBalance(),
+    fileFilter: fileFileter
+  }).array('image', 2);
+
+  upload(req, res, err => {
+    const result = req.files.map(file => {
+      const id = uuid();
+      return {
+        _id: id,
+        url: file.path,
+        name: file.originalname
+      };
+    });
+    result.forEach(image => {
+      db.set(`images.${image._id}`, image).write();
+    });
+    res.json(result);
+  });
+});
+
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const imageData = db.get(`images[${id}]`).value();
+
+  res.json(imageData);
+});
+
+// start the app
+const port = 3000;
+
+app.listen(port, () => console.log(`App is listening on port ${port}.`));
